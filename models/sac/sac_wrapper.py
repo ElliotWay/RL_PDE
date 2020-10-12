9a2d1c28b3b71579971eb616c527d0cbd6f24afc
Elliot Way <eway1@binghamton.edu>
2020-10-12 10:46:36 -0400

    Still working on wrapper for SB-SAC.

--- models/sac/sac_wrapper.py
+++ models/sac/sac_wrapper.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 from stable_baselines.sac import SAC, MlpPolicy, LnMlpPolicy
 from stable_baselines.common import tf_util, OffPolicyRLModel, SetVerbosity, TensorboardWriter
@@ -28,8 +29,6 @@ class SACModel(BaselinesModel):
     another wrapper, though.
     """
     def __init__(self, env, args):
-        self.total_timesteps = args.ep_length * args.total_episodes
-
         policy_cls = LnMlpPolicy if args.layer_norm else MlpPolicy
 
         if args.learning_starts is None:
@@ -52,11 +51,11 @@ class SACModel(BaselinesModel):
 
         sac = self.sac
         sac._setup_learn()
+        self.new_tb_log = sac._init_num_timesteps(True)
         # Transform to callable if needed
-        sac.learning_rate = get_schedule_fn(self.learning_rate)
-        # Initial learning rate
-        current_lr = self.learning_rate(1)
+        sac.learning_rate = get_schedule_fn(sac.learning_rate)
 
+        self.total_timesteps = args.ep_length * args.total_episodes
         self.steps_seen = 0
         self.reward_acc = 0
         self.n_updates = 0
@@ -93,20 +92,20 @@ class SACModel(BaselinesModel):
             # inferred actions need to be transformed to environment action_space before stepping
             unscaled_action = unscale_action(sac.action_space, action)
 
-        return unscaled_action
+        return unscaled_action, None
 
     def train(self, s, a, r, s2, done):
         
         sac = self.sac
 
-        self.steps_seen += len(obs)
+        self.steps_seen += len(s)
         step = self.steps_seen
 
         with SetVerbosity(sac.verbose), \
-             TensorboardWriter(self.graph, self.tensorboard_log, tb_log_name, new_tb_log) \
+             TensorboardWriter(sac.graph, sac.tensorboard_log, "SAC", self.new_tb_log) \
                  as writer:
 
-            self.replay_buffer.extend(s, a, r, s2, done)
+            sac.replay_buffer.extend(s, a, r, s2, done)
 
             # Write reward to tensorboard
             if writer is not None:
@@ -120,13 +119,13 @@ class SACModel(BaselinesModel):
             mb_infos_vals = []
             # Update policy, critics and target networks
             # Compute current learning_rate
-            current_lr = sac.learning_rate(frac)
             frac = min(1.0, 1.0 - step / self.total_timesteps)
+            current_lr = sac.learning_rate(frac)
             for grad_step in range(sac.gradient_steps):
                 # Break if the warmup phase is not over
                 # or if there are not enough samples in the replay buffer
-                if not sac.replay_buffer.can_sample(self.batch_size) \
-                   or step < self.learning_starts:
+                if not sac.replay_buffer.can_sample(sac.batch_size) \
+                   or step < sac.learning_starts:
                     break
                 self.n_updates += 1
                 # Update policy and critics (q functions)
@@ -134,7 +133,7 @@ class SACModel(BaselinesModel):
             # Update target network
             if step - self.prev_target_update > sac.target_update_interval:
                 self.prev_target_update = step
-                self.sess.run(sac.target_update_op)
+                sac.sess.run(sac.target_update_op)
             # Log losses and entropy, useful for monitor training
             if len(mb_infos_vals) > 0:
                 infos_values = np.mean(mb_infos_vals, axis=0)
@@ -143,7 +142,7 @@ class SACModel(BaselinesModel):
 
         info_dict = {'n_updates':n_updates, 'current_lr': current_lr} 
         if len(infos_values) > 0:
-            for (name, val) in zip(self.infos_names, infos_values):
+            for (name, val) in zip(sac.infos_names, infos_values):
                 info_dict[name] = val
 
         return info_dict
