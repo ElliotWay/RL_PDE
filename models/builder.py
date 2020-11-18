5b2434580fb9d6e68ced04ecc736d468daf6b0bd
Elliot Way <eway1@binghamton.edu>
2020-11-18 16:38:55 -0500

    Added model args to run_test.py.
    Why was this not a problem before? This is an ugly hack to get this to
    work.

---
+++ models/builder.py
@@ -0,0 +1,35 @@
+import argparse
+
+def get_model_arg_parser():
+    parser = argparse.ArgumentParser(
+            add_help=False,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--layers', type=int, nargs='+', default=[32, 32],
+            help="Size of network layers.")
+    parser.add_argument('--layer-norm', '--layer_norm', default=False, action='store_true',
+            help="Use layer normalization between network layers.")
+    parser.add_argument('--gamma', type=float, default=0.0,
+            help="Discount factor on future rewards.")
+    parser.add_argument('--learning-rate', '--learning_rate', '--lr', type=float, default=3e-4,
+            help="(some) Learning rate if the model uses only one for all networks.")
+    parser.add_argument('--actor-lr', '--actor_lr', type=float, default=1e-4,
+            help="(some) Learning rate for actor network (pi).")
+    parser.add_argument('--critic-lr', '--critic_lr', type=float, default=1e-3,
+            help="(some) Learning rate for critic network (Q).")
+    parser.add_argument('--buffer-size', '--buffer_size', type=int, default=None,
+            help="Size of the replay buffer. The default is 10000 for std EMI, and 500000"
+            + " otherwise.")
+    parser.add_argument('--batch-size', '--batch_size', type=int, default=64,
+            help="Size of batch samples from replay buffer.")
+    parser.add_argument('--train-freq', '--train_freq', type=int, default=None,
+            help="(SAC) Ratio between the number of steps and the number of times to train."
+            + " The default for std is 1, i.e. train every step. The default otherwise"
+            + " is nx+1, i.e. train every time step.")
+    #parser.add_argument('--noise-type', '--noise_type', type=str, default='adaptive-param_0.2',
+            #help="(DDPG) Noise added to actions.")
+    parser.add_argument('--learning-starts', type=int, default=None,
+            help="(SAC) At what step the learning starts; before this, actions are take at"
+            + " random. Note: a full timestep actually contains dx steps. Default is one episode's"
+            + " worth.")
+ 
+    return parser
