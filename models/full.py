6bb1dda490cd931d4cdf2c39053c73af3ce26bfc
Elliot Way <eway1@binghamton.edu>
2021-02-26 15:02:42 -0500

    Implemented more of the network in TF.
    There was more to do than I thought. The built-in RNN does not handle
    this case, so I implemented it myself. Also implemented the reward
    function, which is involved considering it needs to include computing
    the next state with WENO. That feels wrong, but I don't know any other
    way to do this.

--- models/full.py
+++ models/full.py
@@ -7,14 +7,88 @@ from tf.keras.layers import Layer
 from util.misc import create_stencil_indexes
 import envs.weno_coefficients as weno_coefficients
 
+def makeRNN():
+    policy = make_policy_network()
+    cell = IntegrateCell(grid,
+            prep_state_fn=WENO_prep_state,
+            policy_net=policy,
+            integrate_fn=WENO_integrate
+            )
+    rnn = IntegrateRNN(cell)
+
+
+
+class IntegrateRNN(Layer):
+    def __init__(self, cell, dtype=tf.float32, swap_to_cpu=True):
+        """
+        Declare the RNN for PDE integration.
+        
+        Parameters
+        ----------
+        cell : tf.keras.layers.Layer
+          The recurrent cell. Probably an IntegrateCell.
+          Needs to have a call(previous_state) method returning (action, next_state).
+        dtype : dtype
+          Type of the state (probably tf.float32 or tf.float64).
+        swap_to_cpu : bool
+          Whether to copy Tensors to the CPU from the GPU. Used by the tf.while_loop() function.
+          With long chains, too many Tensors on the GPU will exhaust the memory, but if the chain
+          is relatively short (how short depends on the GPU), you can (probably) increase
+          performance by keeping the Tensors on the GPU instead. True by default. Does nothing if
+          only using the CPU anyway.
+        """
+        super().__init__()
+        self.cell = cell
+        self.dtype = dtype
+        self.swap_to_cpu = swap_to_cpu
+
+    def build(self, input_size):
+        #???
+        super().build()
+
+    def call(initial_state, num_steps):
+        states_ta = tf.TensorArray(dtype=self.dtype, size=num_steps)
+        actions_ta = tf.TensorArray(dtype=self.dtype, size=num_steps)
+
+        def condition_fn(time, *_):
+            return time < num_steps
+
+        def loop_fn(time, states_ta, actions_ta, current_state):
+            next_action, next_state = self.cell(current_state)
+
+            # Overwriting the TensorArrays is necessary to keep connections
+            # through the entire network graph.
+            states_ta = states_ta.write(time, next_state)
+            actions_ta = actions_ta.write(time, next_action)
+
+            return (time + 1, states_ta, actions_ta, next_state)
+
+        initial_time = tf.Constant(0, dtype=tf.int32)
+
+        _time, states_ta, actions_ta, final_state = tf.while_loop(
+                cond=condition_fn,
+                body=loop_fn,
+                loop_vars=(initial_time, states_ta, actions_ta, initial_state),
+                parallel_iterations=10, # Iterations of the loop to run in parallel?
+                                        # I'm not sure how that works.
+                                        # 10 is the default value. Keras backend uses 32.
+                swap_memory=self.swap_to_cpu,
+                return_same_structure=True)
+
+        states = states_ta.stack()
+        actions = actions_ta.stack()
+        return states, actions
+
+
 class IntegrateCell(Layer):
-    def __init__(self, grid, prep_state_fn, policy_net, integrate_fn):
+    def __init__(self, grid, prep_state_fn, policy_net, integrate_fn, reward_fn):
         super().__init__()
         self.grid = grid
 
         self.prep_state_fn = prep_state_fn
         self.policy_net = policy_net
         self.integrate_fn = integrate_fn
+        self.reward_fn = reward_fn
         
     def build(self, input_size):
         # The input_size is irrelevant, right? It only ever depends on the initial state.
@@ -27,8 +101,7 @@ class IntegrateCell(Layer):
 
         super().build()
 
-    def call(inputs, state, **kwargs):
-        assert inputs is None, "inputs should be None - only needed as part of RNN API."
+    def call(state, **kwargs):
 
         # Convert input to RL state.
         real_state = state
@@ -43,8 +116,6 @@ class IntegrateCell(Layer):
         # Return the next real state AND the RL action (otherwise we never see the action).
         return rl_action, next_real_state
 
-    def get_initial_state(inputs=None, batch_size=None, dtype=None):
-        raise NotImplementedError("Integration must have initial state given explicitly.")
 
 def make_policy_network(state_size, action_size):
     #Make e.g. the 32,32 network.
@@ -93,6 +164,8 @@ Based on a combination of functions in envs/burgers_env.py.
 """
 @tf.function
 def WENO_integrate(real_state, rl_state, rl_action):
+    # TODO does real_state contain ghost cells here?
+    # Don't forget that both the cell and the reward function call this.
 
     plus_stencils = rl_state[:, 0]
     minus_stencils = rl_state[:, 1]
@@ -123,4 +196,134 @@ def WENO_integrate(real_state, rl_state, rl_action):
     
     new_state = real_state + step
     return new_state
+
+"""
+Based on a combination of functions in envs/burgers_env.py and in agents.py.
+#TODO Same deal again, could probably put this into the Env.
+"""
+@tf.function
+def WENO_reward(real_state, rl_state, rl_action, next_real_state):
+    # Note that real_state and next_real_state do not include ghost cells.
+
+    # agents.py#StandardWENOAgent._weno_weights_batch()
+    C_values = weno_coefficients.C_all[self.order]
+    C_values = tf.constant(C_values)
+    sigma_mat = weno_coefficients.sigma_all[self.order]
+    sigma_mat = tf.constant(sigma_mat)
+
+    fp_stencils = rl_state[:, 0]
+    fm_stencils = rl_state[:, 1]
+
+    sub_stencil_indexes = create_stencil_indexes(stencil_size=self.order, num_stencils=self.order)
+
+    sub_stencils_fp = tf.reverse(fp_stencils[:, sub_stencil_indexes], axis=-1)
+    sub_stencils_fm = tf.reverse(fm_stencils[:, sub_stencil_indexes], axis=-1)
+
+    q_squared_fp = sub_stencils_fp[:, :, None, :] * sub_stencils_fp[:, :, :, None]
+    q_squared_fm = sub_stencils_fm[:, :, None, :] * sub_stencils_fm[:, :, :, None]
+
+    beta_fp = tf.reduce_sum(sigma_mat * q_squared_fp, axis=(-2, -1))
+    beta_fm = tf.reduce_sum(sigma_mat * q_squared_fm, axis=(-2, -1))
+
+    epsilon = tf.constant(1e-16)
+    alpha_fp = C_values / (epsilon + beta_fp ** 2)
+    alpha_fm = C_values / (epsilon + beta_fm ** 2)
+
+    weights_fp = alpha_fp / (tf.reduce_sum(alpha_fp, axis=-1)[:, None])
+    weights_fm = alpha_fm / (tf.reduce_sum(alpha_fm, axis=-1)[:, None])
+    weno_action = tf.stack([weights_fp, weights_fm], axis=1)
+    # end adaptation of _weno_weights_batch()
+
+    weno_next_real_state = WENO_integrate(real_state, rl_state, WENO_action)
+
+    #envs/burgers_env.py#AbstactBurgersEnv.calculate_reward()
+    if "wenodiff" in self.reward_mode:
+        last_action = rl_action
+
+        action_diff = weno_action - last_action
+        partially_flattened_shape = (self.action_space.shape[0],
+                np.prod(self.action_space.shape[1:]))
+        action_diff = tf.reshape(action_diff, partially_flattened_shape)
+        if "L1" in self.reward_mode:
+            error = tf.reduce_sum(tf.abs(action_diff), axis=-1)
+        elif "L2" in self.reward_mode:
+            error = tf.sqrt(tf.reduce_sum(action_diff**2, axis=-1))
+        else:
+            raise Exception("AbstractBurgersEnv: reward_mode problem")
+
+        return -error
+
+    error = self.solution.get_full() - self.grid.get_full()
+
+    if "one-step" in self.reward_mode:
+        # This version is ALWAYS one-step - the others are tricky to implement in TF.
+    elif "full" in self.reward_mode:
+        raise Exception("Reward mode 'full' invalid - only 'one-step' reward implemented"
+            " in Tensorflow functions.")
+    elif "change" in self.reward_mode:
+        raise Exception("Reward mode 'change' invalid - only 'one-step' reward implemented"
+            " in Tensorflow functions.")
+    else:
+        raise Exception("AbstractBurgersEnv: reward_mode problem")
+
+    if "clip" in self.reward_mode:
+        raise Exception("Reward clipping not implemented in Tensorflow functions.")
+
+    # Average of error in two adjacent cells.
+    if "adjacent" in self.reward_mode and "avg" in self.reward_mode:
+        error = tf.abs(error)
+        combined_error = (error[:-1] + error[1:]) / 2
+        # Assume error beyond boundaries is 0, so error at edge interfaces is error at edge cells.
+        combined_error = tf.concat([error[0] / 2, combined_error, error[-1] / 2], axis=0)
+    # Combine error across the stencil.
+    elif "stencil" in self.reward_mode:
+        # This is trickier and we need to expand the error into the ghost cells.
+        # Still assume the error is 0 for convenience.
+        ghost_error = tf.constant([0.0] * self.ng)
+        full_error = tf.concat([ghost_error, error, ghost_error])
+        
+        stencil_indexes = create_stencil_indexes(
+                stencil_size=(self.weno_order * 2 - 1),
+                num_stencils=(self.nx + 1),
+                offset=(self.ng - self.weno_order))
+        error_stencils = full_error[stencil_indexes]
+        if "max" in self.reward_mode:
+            error_stencils = tf.abs(error_stencils)
+            combined_error = tf.reduce_max(error_stencils, axis=-1)
+        elif "avg" in self.reward_mode:
+            error_stencils = tf.abs(error_stencils)
+            combined_error = tf.reduce_mean(error_stencils, axis=-1)
+        elif "L2" in self.reward_mode:
+            combined_error = tf.sqrt(tf.reduce_sum(error_stencils**2, axis=-1))
+        elif "L1" in self.reward_mode:
+            error_stencils = tf.abs(error_stencils)
+            combined_error = tf.reduce_sum(error_stencils, axis=-1)
+        else:
+            raise Exception("AbstractBurgersEnv: reward_mode problem")
+    else:
+        raise Exception("AbstractBurgersEnv: reward_mode problem")
+
+    # Squash reward.
+    if "nosquash" in self.reward_mode:
+        reward = -combined_error
+    elif "logsquash" in self.reward_mode:
+        epsilon = tf.constant(1e-16)
+        reward = -tf.log(combined_error + epsilon)
+    elif "arctansquash" in self.reward_mode:
+        if "noadjust" in self.reward_mode:
+            reward = tf.atan(-combined_error)
+        else:
+            reward_adjustment = tf.contant(self.reward_adjustment)
+            # The constant controls the relative importance of small rewards compared to large rewards.
+            # Towards infinity, all rewards (or penalties) are equally important.
+            # Towards 0, small rewards are increasingly less important.
+            # An alternative to arctan(C*x) with this property would be x^(1/C).
+            reward = tf.atan(reward_adjustment * -combined_error)
+    else:
+        raise Exception("AbstractBurgersEnv: reward_mode problem")
+
+    # end adaptation of calculate_reward()
+
+    return reward
+
 
