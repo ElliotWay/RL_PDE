a21055796757d9f0d883cead98275d0189a3c865
Elliot Way <eway1@binghamton.edu>
2021-09-24 11:06:35 -0400

    Switch 1D WENO agent to weno_solution.py function.

--- rl_pde/agents/weno_agents.py
+++ rl_pde/agents/weno_agents.py
@@ -43,11 +43,12 @@ class StandardWENO2DAgent(Policy):
 
 class StandardWENOAgent(Policy):
     def __init__(self, order=3, action_type="weno"):
+        print("Using standard WENO agent.")
         self.order = order
         self.action_type = action_type
 
     def predict(self, state, deterministic=False):
-        weno_weights = self._weno_weights_batch(state)
+        weno_weights = weno_weights_nd(state, self.order)
 
         if self.action_type == "weno":
             return weno_weights, None
@@ -71,68 +72,3 @@ class StandardWENOAgent(Policy):
             return flux_weights, None
 
         raise Exception("{} action type not implemented.".format(action_type))
-
-    # Future note: we're wasting some computations.
-    # The q^2 matrix for the kth sub-stencil has a lot of overlap with the q^2 matrix with the k+1st sub-stencil.
-    # Also the upper triangle of the q^2 matrix is not needed.
-    # Not a huge deal, but could be relevant with higher orders or factors of increasing complexity.
-    def _weno_weights_batch(self, q_batch):
-        """
-        Get WENO weights for a batch
-  
-        Parameters
-        ----------
-        q_batch : numpy array
-          Batch of flux stencils of size  grid length + 1 X 2 (fp, fm) X stencil size
-  
-        Returns
-        -------
-        Weights for batch.
-  
-        """
-
-        order = self.order
-
-        C = weno_coefficients.C_all[order]
-        sigma = weno_coefficients.sigma_all[order]
-        epsilon = 1e-16
-
-        fp_stencils = q_batch[:, 0, :]
-        fm_stencils = q_batch[:, 1, :]
-
-        sliding_window_indexes = np.arange(order)[None, :] + np.arange(order)[:, None]
-
-        # beta refers to the smoothness indicator.
-        # To my best understanding, beta is calculated based on an approximation of the square of the values
-        # in q, so we multiply every pair of values in a sub-stencil together, weighted appropriately for
-        # the approximation by sigma.
-
-        sub_stencils_fp = fp_stencils[:, sliding_window_indexes]
-        sub_stencils_fm = fm_stencils[:, sliding_window_indexes]
-
-        # Flipped because in original formulation we subtract from last index instead of adding from first,
-        # and sigma and C weights assume this ordering.
-        # Note: there was a typo in this line since before this file was created (July 2020) until
-        # now (July 2021). Apparently this flip isn't important. I'm not sure why, but there's a
-        # lot of symmetry going on - maybe there's a mathematical reason why they should be almost
-        # the same.
-        sub_stencils_fp = np.flip(sub_stencils_fp, axis=-1)
-        sub_stencils_fm = np.flip(sub_stencils_fm, axis=-1)
-
-        # Insert extra empty dimensions so numpy broadcasting produces
-        # the desired outer product along only the intended dimensions.
-        q_squared_fp = sub_stencils_fp[:, :, None, :] * sub_stencils_fp[:, :, :, None]
-        q_squared_fm = sub_stencils_fm[:, :, None, :] * sub_stencils_fm[:, :, :, None]
-
-        # Note that sigma is made up of triangular matrices, so the second copy of each pair is weighted by 0.
-        beta_fp = np.sum(sigma * q_squared_fp, axis=(-2, -1))
-        beta_fm = np.sum(sigma * q_squared_fm, axis=(-2, -1))
-
-        alpha_fp = C / (epsilon + beta_fp ** 2)
-        alpha_fm = C / (epsilon + beta_fm ** 2)
-
-        # We need the [:, None] so numpy broadcasts to the sub-stencil dimension, instead of the stencil dimension.
-        weights_fp = alpha_fp / (np.sum(alpha_fp, axis=-1)[:, None])
-        weights_fm = alpha_fm / (np.sum(alpha_fm, axis=-1)[:, None])
-
-        return np.stack([weights_fp, weights_fm], axis=1)
