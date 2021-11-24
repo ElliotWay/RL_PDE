72f7e3d2c496b837555fce8b6a8bb4086df1798c
Elliot Way <eway1@binghamton.edu>
2021-11-24 12:08:39 -0500

    Extended combine_time_plots.py to plot mean/CI.

--- scripts/combine_time_plots.py
+++ scripts/combine_time_plots.py
@@ -3,23 +3,41 @@ import argparse
 import re
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from util import plots
 from util.misc import soft_link_directories
+from util.argparse import ExtendAction
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Combine data plotted vs time from earlier test runs into a single plot.",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('files', type=str, nargs='+',
-            help="CSV file containing data. It should have a column labeled 't' or 'time',"
-            + " and exactly one other column (with any label).")
-            #TODO: possibly extend to handle multiple columns in a sensible way.
-    parser.add_argument("--labels", type=str, nargs='+', required=True,
-            help="Labels for the values from each file.")
+        description="Combine data plotted vs time from earlier test runs into a single plot."
+        + "\nThe CSV files must have a column labeled 't' or 'time', and at least one"
+        + "\nother column. The order of arguments controls the order of the legend.",
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument(type=str, nargs='*', action=ExtendAction, dest='curves',
+            metavar='FILE',
+            help="CSV files containing training data. (progress.csv)")
+    parser.add_argument('--files', type=str, nargs='+', action=ExtendAction, dest='curves',
+            metavar='FILE',
+            help="More CSV files containing training data.")
+    parser.add_argument('--avg', type=str, nargs='+', action='append', dest='curves',
+            metavar='FILE',
+            help="CSV files for which the mean with a confidence interval will be plotted.")
+    parser.add_argument('--ci-type', type=str, default='range',
+            help = "The type of confidence interval to plot. (default: range)\n"
+                + " Options are:\n"
+                + "   range: [min,max]\n"
+                + "   Xconf: [P(lower)=(1-X)/2,P(higher)=(1-X)/2] (T dist), X in [0,1]\n"
+                + "   Xsig: [-X std deviations,+X std deviations] (normal dist), X > 0\n"
+                + "   Nperc: [Nth percentile,100-Nth percentile], N in [0, 50]\n")
+    parser.add_argument("--labels", type=str, nargs='+', default=None,
+            help="Labels for each file/average.")
+    parser.add_argument("--ycol", type=str, default=None,
+            help="Name of the column containing data for the y-axis. Can be omitted."
+            + "\nif the files have only one non-time column.")
     parser.add_argument("--ylabel", type=str, default=None,
-            help="Name of the y axis. By default, the name of the non-time column"
-            + " of the first file.")
+            help="Name of the y axis. Defaults to the name of the column with the data.")
     parser.add_argument("--title", type=str, default=None,
             help="Title to add to the plot. By default, no title is added.")
     parser.add_argument("--output", "-o", type=str, required=True,
@@ -28,38 +46,93 @@ def main():
     args = parser.parse_args()
 
     if args.labels is not None:
-        if len(args.labels) != len(args.files):
+        if len(args.labels) != len(args.curves):
             raise Exception(f"Number of labels ({len(args.labels)}) must match"
-                    + f" number of files ({len(args.files)}).")
+                    + f" number of curves to plot ({len(args.curves)}).")
 
     time_values = []
     y_values = []
 
-    for filename in args.files:
-        csv_df = pd.read_csv(filename, comment='#')
-        if not 'time' in csv_df and not 't' in csv_df:
-            raise Exception(f"{filename} has no time data.")
-        if 'time' in csv_df:
-            time_values.append(csv_df['time'])
+    for curve in args.curves:
+        if isinstance(curve, str):
+            is_list = False
         else:
-            time_values.append(csv_df['t'])
+            is_list = True
 
-        other_column_names = [name for name in csv_df if name not in ['time', 't']]
-        if len(other_column_names) == 0:
-            raise Exception(f"{filename} has no data to plot against time.")
-        if len(other_column_names) > 1:
-            raise Exception(f"{filename} should only have one other column.")
+        if not is_list:
+            filename = curve
+        
+            csv_df = pd.read_csv(filename, comment='#')
+            if not 'time' in csv_df and not 't' in csv_df:
+                raise Exception(f"{filename} has no time data.")
+            if 'time' in csv_df:
+                time_values.append(csv_df['time'])
+            else:
+                time_values.append(csv_df['t'])
 
-        value_name = other_column_names[0]
-        y_values.append(csv_df[value_name])
-        if args.ylabel is None:
-            args.ylabel = value_name
+            if args.ycol is None:
+                other_column_names = [name for name in csv_df if name not in ['time', 't']]
+                if len(other_column_names) == 0:
+                    raise Exception(f"{filename} has no data to plot against time.")
+                if len(other_column_names) > 1:
+                    raise Exception(f"Multiple columns in {filename}, specify with --ycol.")
+                else:
+                    args.ycol = other_column_names[0]
+            elif args.ycol not in csv_df:
+                raise Exception(f"Cannot find '{args.ycol}' in {filename}.")
+            y_values.append(csv_df[args.ycol])
 
-    dir_name, file_name = os.path.split(args.output)
-    plots.plot_over_time(time_values, y_values, log_dir=dir_name, name=file_name,
-            ylabel=args.ylabel, labels=args.labels, title=args.title)
+        else:
+            filenames = curve
+            dfs = [pd.read_csv(name, comment='#') for name in filenames]
+            first_time = None
+            for name, df in zip(filenames, dfs):
+                if not 'time' in df and not 't' in df:
+                    raise Exception(f"{name} has no time data.")
+                time_data = df['time' if 'time' in df else 't']
+                if first_time is None:
+                    first_time = time_data
+                elif any(first_time != time_data):
+                    raise Exception(f"Times in {filenames[0]} do not match times in {name}.")
+            time_values.append(first_time)
+
+            if args.ycol is None:
+                other_column_names = [name for name in dfs[0] if name not in ['time', 't']]
+                if len(other_column_names) == 0:
+                    raise Exception(f"{filename} has no data to plot against time.")
+                if len(other_column_names) > 1:
+                    raise Exception(f"Multiple columns in {filename}, specify with --ycol.")
+                else:
+                    args.ycol = other_column_names[0]
+            for name, df in zip(filenames, dfs):
+                if args.ycol not in df:
+                    raise Exception(f"Cannot find '{args.ycol}' in {name}.")
+            y_values.append([df[args.ycol] for df in dfs])
+
+    if args.ylabel is None:
+        args.ylabel = args.ycol
+
+    fig = plots.create_avg_plot(time_values, y_values,
+            labels=args.labels, kwargs_list=None, # Use default matplotlib colors.
+            ci_type=args.ci_type)
+    if args.labels is not None:
+        fig.legend(prop={'size': plots.legend_font_size(len(y_values))})
+    ax = fig.gca()
+    ax.set_xlabel("time")
+    ax.set_xmargin(0.0)
+    ax.set_ylabel(args.ylabel)
+    #ax.set_yscale('log') # Add an arg to the parser for this if we need it.
+    if args.title is not None:
+        ax.set_title(title)
+
+    plt.tight_layout()
+
+    plt.savefig(args.output)
+    print(f"Saved plot to {args.output}.")
+    plt.close()
 
     # Create symlink for convenience.
+    dir_name, file_name = os.path.split(args.output)
     if len(dir_name) > 0:
         log_link_name = "last"
         error = soft_link_directories(dir_name, log_link_name, safe=True)
