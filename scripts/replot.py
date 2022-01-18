150b30bac21ad201ca656897397dbab9a02ff714
Elliot Way <eway1@binghamton.edu>
2022-01-18 12:10:38 -0500

    Updated scripts/replot.py.

--- scripts/replot.py
+++ scripts/replot.py
@@ -2,9 +2,9 @@ import argparse
 import os
 import re
 import yaml
-import pandas
+import pandas as pd
 
-from rl_pde.run import write_summary_plots, write_final_plots
+import util.plots as plots
 from util import metadata
 from util import param_manager
 from util.misc import soft_link_directories
@@ -12,47 +12,33 @@ from util.misc import soft_link_directories
 def main():
     parser = argparse.ArgumentParser(
         description="Regenerate plots based on data collected during training."
-        + " Useful if you've changed the plot functions in some way since the run.",
+        + " Useful if you've changed the plot functions or added extra data since the run.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("meta", type=str,
-            help="Path to the meta file of the experiment for which to replot."
-            + " (The experiment data must be in the same directory.)")
+    parser.add_argument("progress", type=str,
+            help="Path of the progress.csv file containing data to plot.")
+    parser.add_argument("--output-dir", "-o", type=str, default=None,
+            help="Path of the directory into which to write plots."
+            + " By default, add a subdirectory summary_plots into the same"
+            + " directory as the progress.csv file.")
 
     args = parser.parse_args()
-    log_dir, file_name = os.path.split(args.meta)
-    # Could use the log_dir in the meta file, but the files may have moved since then.
 
-    base_meta_name = os.path.basename(args.meta)
+    csv_df = pd.read_csv(args.progress, comment='#')
 
-    if base_meta_name == metadata.META_FILE_NAME:
-        # New file type.
-        open_file = open(args.meta, 'r')
-        meta_dict = yaml.safe_load(open_file)
-        open_file.close()
+    episode_numbers = csv_df['episodes']
+    total_episodes = max(episode_numbers)
 
-        total_episodes = meta_dict['total_episodes']
-        env_type = meta_dict['env']
-        eval_env_type = meta_dict['eval_env']
-        init_type = meta_dict['e']['init_type']
+    if args.output_dir is None:
+        log_dir, _ = os.path.split(args.progress)
+        args.output_dir = os.path.join(log_dir, "summary_plots")
+    os.makedirs(args.output_dir, exist_ok=True)
 
-    else:
-        assert base_meta_name == metadata.OLD_META_FILE_NAME
-        # Old file type.
-        meta_dict = metadata.load_meta_file(args.meta)
-        total_episodes = int(meta_dict['total_episodes'])
-        env_type = meta_dict['env']
-        eval_env_type = meta_dict['eval_env']
-        init_type = meta_dict['init_type']
-
-    summary_plot_dir = os.path.join(log_dir, "summary_plots")
-    os.makedirs(summary_plot_dir, exist_ok=True)
-
-    csv_file_name = os.path.join(log_dir, "progress.csv")
-    csv_df = pandas.read_csv(csv_file_name, comment='#')
     column_names = list(csv_df)
+    # Current name style.
     eval_env_names = [re.match("eval_(.*)_end_l2", name).group(1) for name in column_names
                             if re.match("eval_.*_end_l2", name)]
     if len(eval_env_names) == 0:
+        # Old name style.
         if eval_env_type not in ["std", "custom"]:
             eval_env_names = [init_type]
         else:
@@ -63,8 +49,49 @@ def main():
             else:
                 raise Exception("Unknown eval env names.")
 
-    write_summary_plots(log_dir, summary_plot_dir, total_episodes, eval_env_names)
-    write_final_plots(log_dir, summary_plot_dir, total_episodes, eval_env_names)
+    try:
+        avg_train_reward = csv_df['avg_train_total_reward']
+    except KeyError:
+        avg_train_reward = None
+    try:
+        avg_eval_reward = csv_df['avg_eval_total_reward']
+    except KeyError:
+        avg_eval_reward = None
+    eval_rewards = [csv_df[f"eval_{env_name}_reward"] for env_name in eval_env_names]
+    
+    if avg_train_reward is not None or avg_eval_reward is not None:
+        main_reward_filename = os.path.join(args.output_dir, "rewards.png")
+        plots.plot_reward_summary(main_reward_filename, episodes=episode_numbers,
+                total_episodes=total_episodes, eval_envs=eval_rewards,
+                eval_env_names=eval_env_names, avg_train=avg_train_reward,
+                avg_eval=avg_eval_reward)
+    eval_reward_filename = os.path.join(args.output_dir, "final_rewards.png")
+    plots.plot_reward_summary(eval_reward_filename, episodes=episode_numbers,
+            total_episodes=total_episodes, eval_envs=eval_rewards,
+            eval_env_names=eval_env_names)
+
+    try:
+        avg_train_l2 = csv_df['avg_train_end_l2']
+    except KeyError:
+        avg_train_l2 = None
+    try:
+        avg_eval_l2 = csv_df['avg_eval_end_l2']
+    except KeyError:
+        avg_eval_l2 = None
+    eval_l2s = [csv_df[f"eval_{env_name}_end_l2"] for env_name in eval_env_names]
+    
+    if avg_train_l2 is not None or avg_eval_l2 is not None:
+        main_l2_filename = os.path.join(args.output_dir, "l2.png")
+        plots.plot_l2_summary(main_l2_filename, episodes=episode_numbers,
+                total_episodes=total_episodes, eval_envs=eval_l2s,
+                eval_env_names=eval_env_names, avg_train=avg_train_l2,
+                avg_eval=avg_eval_l2)
+    eval_l2_filename = os.path.join(args.output_dir, "final_l2.png")
+    plots.plot_l2_summary(eval_l2_filename, episodes=episode_numbers,
+            total_episodes=total_episodes, eval_envs=eval_l2s,
+            eval_env_names=eval_env_names)
+
+    print(f"Updated plots in {args.output_dir}.")
 
     # Create symlink for convenience.
     if len(log_dir) > 0:
